10fbf55449b1d5800e37165ed8917c619e6bb73c
Victor Ferreira <116020366+Vitu-Nff@users.noreply.github.com>
2023-06-11 12:15:18 -0300

    [feat] Lista de Tarefas com jQuery

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,54 +1,22 @@
-document.getElementById('form').addEventListener('submit', function(e) {
-  e.preventDefault();
+$(document).ready(function(){
+    $("#taskForm").submit(function(event){
+        event.preventDefault();
+        var task = $("#taskInput").val();
+        $("#pendingTasks").prepend('<li>'+task+'</li>');
+        $("#taskInput").val('');
+    });
 
-  var campoA = document.getElementById('campoA').value;
-  var campoB = document.getElementById('campoB').value;
-  var message = document.getElementById('message');
+    $(document).on('click', '#pendingTasks li', function(){
+        $(this).toggleClass('done');
+        if ($(this).hasClass('done')) {
+            $(this).detach().appendTo('#completedTasks');
+        }
+    });
 
-  if (campoB > campoA) {
-      message.textContent = "Formulário válido.";
-      message.classList.remove("error");
-      message.classList.add("success");
-      message.classList.remove("hide");
-  } else {
-      message.textContent = "Formulário inválido. Verifique se o 2º Número é maior que o 1º Número.";
-      message.classList.remove("success");
-      message.classList.add("error");
-      message.classList.remove("hide");
-  }
+    $(document).on('click', '#completedTasks li', function(){
+        $(this).toggleClass('done');
+        if (!$(this).hasClass('done')) {
+            $(this).detach().prependTo('#pendingTasks');
+        }
+    });
 });
-
-document.getElementById('campoA').addEventListener('keyup', function(e) {
-  updateHints()
-})
-document.getElementById('campoA').addEventListener('focus', function(e) {
-  updateHints()
-})
-document.getElementById('campoB').addEventListener('keyup', function(e) {
-  updateHints()
-})
-document.getElementById('campoB').addEventListener('focus', function(e) {
-  updateHints()
-})
-
-function updateHints() {
-  var campoA = document.getElementById('campoA').value;
-  var campoB = document.getElementById('campoB').value;
-  var hintA = document.getElementById('hintCampoA');
-  var hintB = document.getElementById('hintCampoB');
-  
-  if(!campoB) {
-    hintA.style.color = "black";
-    hintB.style.color = "black";
-    return
-  }
-
-  if( campoB <= campoA ) {
-    hintA.style.color = "red";
-    hintB.style.color = "red";
-  }
-  else {
-    hintA.style.color = "black";
-    hintB.style.color = "black";
-  }
-}
