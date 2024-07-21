a6e7f43655130e94cda69ed6de94706e91005428
Vitu-Nff <vitu.nff@gmail.com>
2024-07-21 17:35:30 -0300

    [chore] Reset do Repo

--- src/js/scripts-home.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var button = document.createElement('button');
-  button.textContent = 'Clique aqui!';
-  document.body.appendChild(button);
-
-  button.addEventListener('click', function() {
-    alert('Clicado!!');
-  });
-})();
--- src/js/scripts.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  console.log('Hello, world!');
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var element = document.createElement('div');
-    element.textContent = 'Elemento dinâmico!';
-    document.body.appendChild(element);
-  });
-})();
